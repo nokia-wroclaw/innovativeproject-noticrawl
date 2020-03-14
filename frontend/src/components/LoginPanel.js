c177a242bc0c28ac989b998e96d9d97fc40596bb
Konrad Prusak <61755751+konradprusak@users.noreply.github.com>
2020-03-14 02:16:21 +0100

    feat: login page

---
+++ frontend/src/components/LoginPanel.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function LoginPanel() {
+    return (
+        <loginpanel className="LoginPanel">
+            <h3>Zaloguj się</h3>
+
+            
+
+        </loginpanel>
+    )
+}
+
+export default LoginPanel
