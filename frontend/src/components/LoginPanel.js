ec5d3bcd1c43bb7dfc940b70737b4b9153b48b16
Konrad Prusak <61755751+konradprusak@users.noreply.github.com>
2020-03-14 15:45:00 +0100

    added: login panel

--- frontend/src/components/LoginPanel.js
+++ frontend/src/components/LoginPanel.js
@@ -1,14 +1,58 @@
 import React from "react"
+import '../App.css'
 
-function LoginPanel() {
-    return (
-        <loginpanel className="LoginPanel">
-            <h3>Zaloguj się</h3>
+import LoginBox from "./LoginBox"
+import RegisterBox from "./RegisterBox"
 
-            
+class LoginPanel extends React.Component {
 
-        </loginpanel>
-    )
+    constructor(props) {
+        super(props);
+        this.state = {
+          isLoginOpen: true,
+          isRegisterOpen: false
+        };
+    }
+
+     showLoginBox() {
+        this.setState({isLoginOpen: true, isRegisterOpen: false});
+        }
+        
+     showRegisterBox() {
+        this.setState({isRegisterOpen: true, isLoginOpen: false});
+        }
+
+        render() {
+            return (
+                <div className="root-container">
+                  <div className="box-controller">
+                      <div
+                          className={"controller " + (this.state.isLoginOpen
+                          ? "selected-controller"
+                          : "")}
+                          onClick={this
+                          .showLoginBox
+                          .bind(this)}>
+                          Login
+                      </div>
+                      <div
+                          className={"controller " + (this.state.isRegisterOpen
+                          ? "selected-controller"
+                          : "")}
+                          onClick={this
+                          .showRegisterBox
+                          .bind(this)}>
+                          Register
+                      </div>
+                  </div>
+                  <div className="box-container">
+                  {this.state.isLoginOpen && <LoginBox />}
+                  {this.state.isRegisterOpen && <RegisterBox />}
+                  </div>
+              </div>
+            );
+        }
+      
 }
 
-export default LoginPanel
+export default LoginPanel  
