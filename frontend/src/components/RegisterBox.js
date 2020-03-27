f213084ff76cff98efc92fe8140f81e1c43cfbf9
Dawid <arehcats@gmail.com>
2020-03-27 22:25:26 +0100

    feat: confirma of passwords, acceptance of terms

--- frontend/src/components/RegisterBox.js
+++ frontend/src/components/RegisterBox.js
@@ -1,23 +1,65 @@
 import React from "react"
 import '../App.css'
+import { Formik, Form, useField } from "formik";
+import * as Yup from "yup";
 
-const validEmailRegex = RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
-const validateForm = (errors) => {
-  let valid = true;
-  Object.values(errors).forEach(
-    (val) => val.length > 0 && (valid = false)
+
+
+const MyTextInput = ({ label, ...props }) => {
+  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+  // which we can spread on <input> and alse replace ErrorMessage entirely.
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className="text-input" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
   );
-  return valid;
-}
+};
+
+const MyCheckbox = ({ children, ...props }) => {
+  const [field, meta] = useField({ ...props, type: "checkbox" });
+  return (
+    <>
+      <label className="checkbox">
+        <input {...field} {...props} type="checkbox" />
+        {children}
+      </label>
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
+
+
+const MySelect = ({ label, ...props }) => {
+  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+  // which we can spread on <input> and alse replace ErrorMessage entirely.
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <div htmlFor={props.id || props.name}>{label}</div>
+      <div {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div>{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
 
 class RegisterBox extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      username: null,
-      email: null,
-      password: null,
+      username: "",
+      password: "ss",
+      email: "",
+      acceptedTerms: false,
       errors: {
         username: '',
         email: '',
@@ -28,83 +70,75 @@ class RegisterBox extends React.Component {
     
   }
   
-  handleChange = (event) => {
-    event.preventDefault();
-    const { name, value } = event.target;
-    let errors = this.state.errors;
-
-    switch (name) {
-      case 'username': 
-        errors.username = 
-          value.length < 5
-            ? 'Username must be 5 characters long!'
-            : '';
-        break;
-      case 'email': 
-        errors.email = 
-          validEmailRegex.test(value)
-            ? ''
-            : 'Email is not valid!';
-        break;
-      case 'password': 
-        errors.password = 
-          value.length < 8
-            ? 'Password must be 8 characters long!'
-            : '';
-        break;
-      default:
-        break;
-    }
-
-    this.setState({errors, [name]: value});
-  }
-    
-  handleSubmit = (event) => {
-    event.preventDefault();
-    if(validateForm(this.state.errors)) {
-      console.info('Valid Form')
-      const data = new FormData(event.target);
-    
-      fetch('/api/form-submit-url', {
-        method: 'POST',
-        body: data,
-      });
-    }else{
-      console.error('Invalid Form')
-    }
-  }
-  
   render() {
     const {errors} = this.state;
     return (
         <div className="inner-container">
           
           <div className="box">
-          <form onSubmit={this.handleSubmit} noValidate>
-            <div className="input-group">
-              <label htmlFor="username">Username</label>
-              <input type='text' name='username' className="login-input" placeholder="Username" onChange={this.handleChange} noValidate />
-              {errors.username.length > 0 && 
-                <span className='error'>{errors.username}</span>}
-            </div>
-  
-            <div className="input-group">
-              <label htmlFor="email">Email</label>
-              <input type='email' name='email' className="login-input" placeholder="Email" onChange={this.handleChange} noValidate />
-              {errors.email.length > 0 && 
-              <span className='error'>{errors.email}</span>}
-            </div>
-  
-            <div className="input-group">
-              <label htmlFor="password">Password</label>
-              <input type='password' name='password' className="login-input"
-                placeholder="Password" onChange={this.handleChange} noValidate />
-              {errors.password.length > 0 && 
-                <span className='error'>{errors.password}</span>}
-            </div>
-            <button
-              className="login-btn">Register</button>
-          </form>
+
+          <Formik
+        initialValues={{
+          email: "",
+          password: "",
+          confirmPassword: "",
+          acceptedTerms: false, // added for our checkbox
+        }}
+        validationSchema={Yup.object({
+          password: Yup.string()
+            .max(30, "Must be 30 characters or less")
+            .required("Password required")
+            .min(8, "minimum length 8"),
+            confirmPassword: Yup.string()
+            .required("Password required")
+            .oneOf([Yup.ref('password'), null], 'Passwords must match'),
+          email: Yup.string()
+            .email("Invalid email addresss`")
+            .required("Email required"),
+          acceptedTerms: Yup.boolean()
+            .required("Required")
+            .oneOf([true], "You must accept the terms and conditions."),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        <div className="scroll">
+        <Form>
+        <div className="input-group">
+        <MyTextInput
+            label="Email Address"
+            type='email' 
+            name='email' 
+            className="login-input" 
+            placeholder="Email"
+          />
+          <MyTextInput
+            label="Password"
+            type='password' 
+            name='password' 
+            className="login-input"
+            placeholder="Password"
+          />
+          <MyTextInput
+            label="Confirm password"
+            type='password' 
+            name='confirmPassword' 
+            className="login-input"
+            placeholder="Password"
+          />
+          <MyCheckbox name="acceptedTerms">
+            I accept the terms and conditions
+          </MyCheckbox>
+
+          <button className="login-btn" type="submit">Submit</button>
+          </div>
+        </Form>
+        </div>
+      </Formik>
           </div>
         </div>
       );
