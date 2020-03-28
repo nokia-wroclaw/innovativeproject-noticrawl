2c58a3fb06eae65efddba63aed5a6dcb2b308c91
Magdalena Jasianek <jasianek.magdalena@gmail.com>
2020-03-28 13:06:44 +0100

    build noticrawl static

---
+++ backend/Frontend_Backend/noticrawl.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="build2/static"), name="static2")
+templates = Jinja2Templates(directory="build2")
+
+#Link
+@app.get("/app/v1/link")
+def read_item(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+app.mount("/static", StaticFiles(directory="build/static"), name="static")
+templates2 = Jinja2Templates(directory="build")
+
+#Login
+@app.get("/app/v1/login")
+def read_item(request: Request):
+    return templates2.TemplateResponse("index.html", {"request": request})
+
+
