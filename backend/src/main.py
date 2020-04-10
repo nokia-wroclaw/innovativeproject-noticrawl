6241d3d81e1bed5a5463f3ce14e739eecb961c33
Mjasianek <57919532+Mjasianek@users.noreply.github.com>
2020-04-10 23:29:07 +0200

    Resolve: "2 backend project setup" (#20)
    * Revert "test: test 1"
    
    This reverts commit 798ffb1060eebc157c99cd1a4e25ae4a0648952e.
    
    * build: endpoints
    
    * buid: setup poetry
    
    * build: reorganize project structure
    
    * build: database class
    
    * build: database class
    
    * build static test
    
    * build noticrawl static
    
    * buid(docker): create docker container with backend (NOT WORKING)
    
    * build(docker): fix 0.1.1 (NOT WORKING)
    
    * refactor(docker): still not working
    
    * build(docker): minor fixes STILL NOT WORKING
    
    * refactor: noticrawl static
    
    * DOCKER IS WORKING!!!
    
    * docs(backend): add running instructions
    
    * refactor(backend): change headlines sizes
    
    * docs(backend): add api link
    
    * feat(backend): add showing page from statics
    
    * feat(backend): add route to main page
    
    * fix(backend): add statics
    
    * "refactor: remove test statics
    
    * refactor(backend): cleanup
    
    Co-authored-by: Adrian Płatos <adiplatos@gmail.com>

---
+++ backend/src/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+#FastAPI instance
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str = None
+    description: str = None
+
+app.mount("/static", StaticFiles(directory="/frontend/build/static"), name="static")
+templates = Jinja2Templates(directory="/frontend/build")
+
+@app.get("/api/v1")
+def show_statics(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/")
+def hello_world():
+    return {"message": "Hello World"}
