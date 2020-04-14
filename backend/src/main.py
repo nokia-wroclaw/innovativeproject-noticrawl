5fc343f0c11e4a1a29c6fb376b2ef4a5ddd3a9b0
bartsvoboda <bartlomiejswoboda@gmail.com>
2020-04-14 15:09:18 +0200

    fix(backend): change static files route

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,6 +4,8 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+
+templates = Jinja2Templates(directory="/frontend/build/")
 #FastAPI instance
 app = FastAPI()
 
@@ -12,12 +14,12 @@ class Item(BaseModel):
     description: str = None
 
 app.mount("/static", StaticFiles(directory="/frontend/build/static"), name="static")
-templates = Jinja2Templates(directory="/frontend/build")
 
-@app.get("/api/v1")
+
+@app.get("/")
 def show_statics(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.get("/")
+@app.get("/hello")
 def hello_world():
     return {"message": "Hello World"}
