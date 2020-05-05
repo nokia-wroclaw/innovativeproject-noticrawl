d02175c0a71b886d22a14a88f0a1eec350428d89
Mjasianek <jasianek.magdalena@gmail.com>
2020-05-05 12:14:26 +0200

    refactor: update xpath route

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,37 +1,27 @@
-from typing import List
-import logging
 from fastapi import FastAPI, Request
 from pydantic import BaseModel, BaseConfig
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
-from pyppeteer import launch
-#from backend.src.parse_module import parse
 
 from .database import models
 from .database.connection import SessionLocal
 from .user.user_controller import user_router
+from .parse_module import parse, data_selector
+
+#change class name:
+class Data(BaseModel):
+    link: str
+
+class CrawlingData(BaseModel):
+    email: str
+    xpath: str
+    time: int
 
 app = FastAPI()
 app.include_router(user_router)
 
 models.create_models()
-
-async def parse(url):
-    logging.getLogger("websockets").setLevel("WARN")
-    browser = await launch(headless = True, args = ["--no-sandbox"], logLevel = "WARN")
-    page = await browser.newPage()
-    await page.goto(url)
-    html = await page.content()
-    await browser.close()
-    return html
-
-class Selector(BaseModel):
-    path: str
-
-class Data(BaseModel):
-    link: str
-
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="../frontend/build/static"), name="static")
@@ -47,17 +37,19 @@ def show_statics(request: Request):
 async def post_link(url: Data):
     url_dict = url.dict()
     parsed_link = await parse(url.link)
-    #f = open("page_html.txt", "r", encoding="utf-8")
-    #parsed_link = f.read()
     url_dict.update({"parsedPage": parsed_link})
     return url_dict
 
-@app.post("/api/v1/start-crawling")
-async def post_xpath(selector: Selector):
-    selector_dict = selector.dict()
-    selector_xpath = selector.path
-    selector_dict.update({"parsedPage": selector_xpath})
-    return selector_dict
+#change url
+@app.post("api/v1/crawling-data")
+async def post_crawling_data(crawling_data: CrawlingData):
+    crawling_data_dict = crawling_data.dict()
+    email = crawling_data.email
+    time = crawling_data.time
+    xpath_value = data_selector("http://www.mojapogoda.com/", crawling_data.xpath)
+    crawling_data_dict.update({"email": email, "xpath": xpath_value, "time": time})
+    return {"Status": "WOW"}
+
 
 @app.get("/hello")
 def hello_world():
