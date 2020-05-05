75c3d41ebfdfc4cba40da04e9dfd5a5c2c486b15
Mjasianek <jasianek.magdalena@gmail.com>
2020-05-05 12:32:01 +0200

    refactor: add --no-sandbox

--- backend/src/parse_module.py
+++ backend/src/parse_module.py
@@ -2,10 +2,11 @@ import pyppeteer
 import logging
 import asyncio
 
+
 #Parse page
 async def parse(url):
     logging.getLogger("websockets").setLevel("WARN")
-    browser = await pyppeteer.launch(headless=True, args=["--no-sandbox"], logLevel="WARN")
+    browser = await pyppeteer.launch(headless=True, args=["--no-sandbox"],  logLevel="WARN")
     page = await browser.newPage()
     await page.goto(url)
     html = await page.content()
@@ -16,7 +17,7 @@ async def parse(url):
 #CSS selector
 async def css_selector(url):
     logging.getLogger("websockets").setLevel("WARN")
-    browser = await pyppeteer.launch(headless=False, args=["--no-sandbox"], logLevel="WARN")
+    browser = await pyppeteer.launch(headless=True, args=["--no-sandbox"], logLevel="WARN")
     page = await browser.newPage()
     await page.goto(url, waitUntil='networkidle2')
     element = await page.querySelector('.bTbEzd > div:nth-child(2) > a:nth-child(2)')
@@ -32,23 +33,23 @@ async def css_selector(url):
 #websocket 8.0 -> 6.0
 async def xpath_selector(url):
     logging.getLogger("websockets").setLevel("WARN")
-    browser = await pyppeteer.launch(headless=True, logLevel="WARN")
+    browser = await pyppeteer.launch(headless=True, args=["--no-sandbox"], logLevel="WARN")
     page = await browser.newPage()
     await page.goto(url, waitUntil='networkidle2', timeout=100000)
-    await page.waitForXPath('//*[@id="content_middle_upper_area"]/ul/li[2]/div[2]/ul[2]/li/p')
-    xpath = await page.xpath('//*[@id="content_middle_upper_area"]/ul/li[2]/div[2]/ul[2]/li/p')
+    await page.waitForXPath('/html/body/div[4]/main/div[3]/div/ul/li/img')
+    xpath = await page.xpath('/html/body/div[4]/main/div[3]/div/ul/li/img')
     text = await page.evaluate('(xpath) => xpath.textContent', xpath[0])
     await page.close()
     await browser.close()
     return text
 
-#print(asyncio.get_event_loop().run_until_complete(xpath_selector("http://www.mojapogoda.com/")))
+print(asyncio.get_event_loop().run_until_complete(xpath_selector("https://github.com/")))
 
 
 #data_selector
 async def data_selector(url, xpath):
     logging.getLogger("websockets").setLevel("WARN")
-    browser = await pyppeteer.launch(headless=False, logLevel="WARN")
+    browser = await pyppeteer.launch(headless=True, args=["--no-sandbox"], logLevel="WARN")
     page = await browser.newPage()
     await page.goto(url, waitUntil='networkidle2', timeout=100000)
     await page.waitForXPath(xpath)
