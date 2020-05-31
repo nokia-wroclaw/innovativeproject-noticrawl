8ecfe2db78ce8b9a4fa29dc043295a9112eb94c6
arehcats <61759796+arehcats@users.noreply.github.com>
2020-05-31 15:28:30 +0200

    fix(front): repaired login button (#46)

--- frontend/build/precache-manifest.7e11558ba44f2b99521cf90c85733b73.js
+++ frontend/build/precache-manifest.7e11558ba44f2b99521cf90c85733b73.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ea91ce6480e5df7b679d1d95aa0d7ca7",
+    "revision": "b9ede0dffc24b898ef8a6b69c088c3f4",
     "url": "/index.html"
   },
   {
-    "revision": "6e6b9b7cf5021f3516ad",
-    "url": "/static/css/main.8cd26970.chunk.css"
+    "revision": "54676814afc09b664fdc",
+    "url": "/static/css/main.bf9fe4f8.chunk.css"
   },
   {
-    "revision": "a1ca15e31dbd33f2040f",
-    "url": "/static/js/2.ec9015b1.chunk.js"
+    "revision": "e25555824d17bb4f8a1c",
+    "url": "/static/js/2.bbb10c1f.chunk.js"
   },
   {
     "revision": "74f5aa99b54b84cfa9c524c401386337",
-    "url": "/static/js/2.ec9015b1.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.bbb10c1f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6e6b9b7cf5021f3516ad",
-    "url": "/static/js/main.8b8c5d6e.chunk.js"
+    "revision": "54676814afc09b664fdc",
+    "url": "/static/js/main.b1da01e7.chunk.js"
   },
   {
     "revision": "1dee3d021e727257ae82",
