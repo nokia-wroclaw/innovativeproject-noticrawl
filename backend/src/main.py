7684b9dbf0dd9eca353a749d625a43b5bbf64e2f
Adrian Płatos <adiplatos@gmail.com>
2020-06-02 03:30:53 +0200

    feat(auth): add auth to statics

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,17 +1,23 @@
 import asyncio
+import logging
 
-from fastapi import FastAPI, Request
+from fastapi import Depends, FastAPI, HTTPException, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+from .auth.auth_controller import auth_router
 from .crawling import scheduler
 from .crawling.crawling_controller import crawling_router
 from .database import database_schemas
+from .helpers.oauth2_scheme import oauth2_scheme
 from .user.user_controller import user_router
+
+logger = logging.getLogger("Noticrawl")
 
 app = FastAPI()
 app.include_router(user_router)
 app.include_router(crawling_router)
+app.include_router(auth_router)
 
 database_schemas.create()
 
@@ -21,6 +27,11 @@ app.mount("/static", StaticFiles(directory="../frontend/build/static"), name="st
 templates = Jinja2Templates(directory="../frontend/build")
 
 
+@app.get("/")
+@app.get("/404")
+def show_public_statics(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
 @app.get("/.*")
-def show_statics(request: Request):
+def show_private_statics(request: Request, token=Depends(oauth2_scheme)):
     return templates.TemplateResponse("index.html", {"request": request})
