3ede3f041f7d75665f32de40141dd7c251bb800b
Mjasianek <jasianek.magdalena@gmail.com>
2020-06-02 23:26:53 +0200

    refactor: main_notifications cleanup

--- backend/src/notifications/main_notifications.py
+++ backend/src/notifications/main_notifications.py
@@ -4,31 +4,25 @@ from backend.src.notifications.email_config import EMAIL_ADDRESS, EMAIL_PASSWORD
 from email.message import EmailMessage
 from fastapi import HTTPException
 
-def send_email(send_to, image):
-    with open(image, 'rb') as f:
+
+def send_email(send_to, screenshot, subject, email_text):
+    with open(screenshot, 'rb') as f:
         file_data = f.read()
         file_type = imghdr.what(f.name)
         file_name = f.name
 
     msg = EmailMessage()
-    msg["Subject"] = 'Noticrawl notification'
-    msg["From"] = EMAIL_ADDRESS
+    msg["Subject"] = subject
+    msg["From"] = email_address
     msg["To"] = send_to
-    msg.set_content("Hello, I'm Noticrawl email. Have a nice day")
-    msg.add_alternative("""\
-    <!DOCTYPE html>
-    <html>
-        <body>
-            <h1 style="color:Steelblue;">Your śledzik has changed!</h1>
-        </body>
-    </html>    
-    """, subtype='html')
+    msg.add_alternative(email_text, subtype='html')
     msg.add_attachment(file_data, maintype='image', subtype=file_type, filename=file_name)
+
     try:
         server = smtplib.SMTP('smtp.gmail.com:587')
         server.ehlo()
         server.starttls()
-        server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        server.login(email_address, email_password)
         server.send_message(msg)
         server.quit()
         raise HTTPException(status_code=200, detail="Success: Email sent!")
