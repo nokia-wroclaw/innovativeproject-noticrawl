22cc2082411a51554bea9dbe64f146cf03e3ee11
Konrad Prusak <conrezo@gmail.com>
2020-06-10 00:52:15 +0200

    Resolve "Register user and login user"  (#55)
    * feat(frontend):  connect login and register to backend
    
    * refactor (frontend): change 404 path to /error/404
    
    * feat(front): register Error handling
    
    * feat: handle error codes on NewCrawl page
    
    * feat(front): error handling on Login page
    
    * feat: handle error codes on Crawling page
    
    * feat(frontend): connect logout button to backend
    
    * feat(front): login form requires data
    
    * refactor(login): change username field to email
    
    * git(gitignore): fix Login.js in gitignore
    
    Co-authored-by: Dawid <arehcats@gmail.com>

--- frontend/src/components/LoginRegisterPage/Login.js
+++ frontend/src/components/LoginRegisterPage/Login.js
@@ -1,54 +1,112 @@
 import React from "react"
 import { Link } from "react-router-dom";
+import qs from "qs";
 
 
 class LoginBox extends React.Component {
 
-    constructor(props) {
-      super(props);
-      this.state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      error: ""
+    };
+  }
+
+
+  submitLogin = async () => {
+
+    let username = document.getElementById("login_values").username.value
+    let password = document.getElementById("login_values").password.value
+    if (username == "" || password == "") {
+      this.setState({ error: "Enter login details" })
+      return
     }
-  
-    submitLogin(e) {}
-  
-    render() {
-      return (
-        <div className="inner-container">
-          <div className="box">
-  
+    const res = await fetch("/api/v1/login", {
+      method: "POST",
+      body: qs.stringify({ username: username, password: password }),
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+    });
+
+    if (res.ok) {
+      document.getElementById("go_to_new-crawl_login").click()
+    }
+    else if (res.status == 400) {
+      this.setState({ error: "Incorrect username or password" })
+    }
+    else if (res.status == 422) {
+      this.setState({ error: "Validation Error" })
+    }
+    else {
+      this.setState({ error: "Oops, something went wrong" })
+    }
+
+  }
+  render() {
+    return (
+      <div className="inner-container">
+        <div className="box">
+          <form id="login_values">
             <div className="input-group">
-              <label htmlFor="username">Username</label>
+              <label htmlFor="username">Email</label>
               <input
-                type="text"
-                name="username"
+                label="Email Address"
+                type='email'
+                name='username'
                 className="login-input"
-                placeholder="Username"/>
-            </div>
-  
-            <div className="input-group">
+                placeholder="Email"
+              />
               <label htmlFor="password">Password</label>
               <input
-                type="password"
-                name="password"
+                label="Password"
+                type='password'
+                name='password'
                 className="login-input"
-                placeholder="Password"/>
+                placeholder="Password"
+              />
+              <button className="login-btn" type="button" value="Submit" onClick={this.submitLogin}>Login</button>
             </div>
-   
-                <Link to="/new-crawl"> 
-                  <button
-                    type="button"
-                    className="login-btn"
-                    onClick={this
-                    .submitLogin
-                    .bind(this)}>Login</button>
-                </Link>
-
+          </form>
+          <div className="error">{this.state.error}</div>
+          {/* 
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              className="login-input"
+              placeholder="Username" />
           </div>
-        </div>
-      );
-    }
-  
-  } 
 
-  export default LoginBox
- 
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              className="login-input"
+              placeholder="Password" />
+          </div> */}
+
+          {/* <Link to="/new-crawl">
+            <button
+              type="button"
+              className="login-btn"
+              onClick={this
+                .submitLogin
+                .bind(this)}>Login</button>
+          </Link> */}
+          <Link to="/new-crawl">
+            <div id="go_to_new-crawl_login"></div>
+          </Link>
+
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default LoginBox
