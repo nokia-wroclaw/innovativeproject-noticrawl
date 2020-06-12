ed1d1a154e10de7b521ad50cd904dd652f5098aa
conrezo <conrezo@gmail.com>
2020-06-11 17:15:34 -0700

    feat (MyCrawls): list of crawls (v.0.2)

--- frontend/src/components/ContentPages/MyCrawlsPage/Crawl.js
+++ frontend/src/components/ContentPages/MyCrawlsPage/Crawl.js
@@ -3,6 +3,7 @@ import React from "react"
 function Crawl(props) {
     return (
         <div>
+            <h3>Name: {props.name}</h3>
             <h3>Link: {props.link}</h3>
             <hr/>
         </div>
--- frontend/src/components/ContentPages/MyCrawlsPage/MyCrawlsContent.js
+++ frontend/src/components/ContentPages/MyCrawlsPage/MyCrawlsContent.js
@@ -10,22 +10,43 @@ class MyCrawlsContent extends Component {
         }
     }
     
-    componentDidMount() {
-        this.setState({loading: true})
-        fetch("./api/v1/crawling-data/get-crawl-list")
-            .then(response => response.json())
-            .then(response => {
-                const {takeCrawls} = response.data
-                this.setState({
-                    loading: false,
-                    crawlList: takeCrawls
-                })
-            })
-    }
+
+
+
+async componentDidMount() {
+    console.log("rozpoczeto")
+
+    this.setState({loading: true})
+
+    const response = await fetch("./api/v1/crawling-data/me")
+    console.log(response)
+    const json = await response.json()
+    console.log(json)
+
+    const response2 = await fetch("./api/v1/user/me")
+    console.log(response2)
+    const json2 = await response2.json()
+    console.log(json2)
+
+    this.setState({
+        loading: false,
+        crawlList: json
+    })
+
+    /*
+    this.setState({
+        loading: false,
+        crawlList: [{"name": "elo", "link": "http://wp.pl", "crawl_id": 1},
+        {"name": "mordo", "link": "http://onet.pl", "crawl_id": 2}
+        ]
+    })
+    */   
+         
+}
     
     render() {
         const crawls =  this.state.crawlList;
-        const crawlsList = crawls.map(crawl => <Crawl key={crawl.id} link={crawl.link_id} />)
+        const crawlsList = crawls.map(crawl => <Crawl key={crawl.crawl_id} name={crawl.name} link={crawl.link} />)
         //const loader = this.state.loading ? "loading..." : this.state.crawlList.link
         return (
             <div className="PageContent">
