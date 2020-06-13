6d29448df07b89f0d616455bd99469fb797536aa
conrezo <conrezo@gmail.com>
2020-06-12 17:49:37 -0700

    feat (MyCrawls): add edit and delete icons

--- frontend/src/components/ContentPages/MyCrawlsPage/Crawl.js
+++ frontend/src/components/ContentPages/MyCrawlsPage/Crawl.js
@@ -1,14 +1,23 @@
 import React from "react"
+import deleteIcon from "../../../media/bin-icon.png"
+import editIcon from "../../../media/edit-icon.png"
+
 
 function Crawl(props) {
     return (
         <div>
             <hr/>
+            <div className="MyCrawlsElements">
                 <div className="MyCrawlsText">
-                    <text>Crawl Name: {props.name}</text>
+                    <text>Name: {props.name}</text>
                     <br />
                     <text><a href={props.link}>{props.link}</a></text>
                 </div>
+                <div className="MyCrawlsIcons">
+                    <img src={editIcon} alt="Edit" height="34" width="34"/>
+                    <img src={deleteIcon} alt="Delete" height="35" width="35" />
+                </div>
+            </div>
             <hr/>
         </div>
     )
