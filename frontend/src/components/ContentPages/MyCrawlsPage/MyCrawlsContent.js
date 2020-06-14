b19e4b4652c0bcf60860add488f80af5f2d9a903
Konrad Prusak <conrezo@gmail.com>
2020-06-14 11:41:21 -0700

    Resolve "My Crawls Page" (#54)
    * feat(user): add (fake) login()
    
    * build: add passlib dependency
    
    * feature(auth): add real login
    
    * feature(auth): add cookies for login route
    
    * docker: add auth secrets
    
    * feat(auth): add auth scheme
    
    * feat(auth): add auth
    
    * feat: add save_to_txt helper function
    
    * feat(auth): add auth to crawling routes
    
    * feat(auth): add auth to statics
    
    * fix(auth): throw 401 in bearer_cookie_auth
    
    * refactor(auth): combine verify_token with oauth2_scheme
    
    * refactor: remove scrappy junk
    
    * feat(auth): add logout
    
    * refactor: remove logging
    
    * refactor: add newline in debug.py
    
    * feat: add crawl to database
    
    * feat: get crawls from database
    
    * feat: list of crawls (v0.1, only links)
    
    * feat: add read_crawls
    
    * feat: add verify_token dependencies
    
    * feat: add update_crawl
    
    * refactor: cleanup
    
    * refactor: cleanup
    
    * feat (crawling): user can now set name to crawl
    
    * fix(backend): remove excessive auth router in main
    
    * refactor: styling changes in auth_contorller
    
    * fix(crawling): fix adding new crawl
    
    * fix(db): fix relations names to work with pydantic
    
    * fix(crawls): fix getting crawls list
    
    * fix(crawlingCRUD): fix updating a crawl
    
    * refactor(cawrCRUD): remove logger from add_crawl
    
    * refactor(crawling): add api tags to crawling_controller
    
    * feat (MyCrawls): list of crawls (v.0.2)
    
    * fix(crawl CRUD): fix adding crawls
    
    * fix(crawl CRUD): add crawl to user, not email from crawl
    
    * fix(crawling): fix timeout when adding crawl
    
    * fix (crawl): send period in seconds
    
    * feat(crawl CRUD): add deleting crawls
    
    * feat (MyCrawls): list of crawls (v0.3)
    
    * feat (MyCrawls): add edit and delete icons
    
    * style (MyCrawls): change separator and icon design
    
    * feat (MyCrawls): add crawl edit  form (v0.1)
    
    * feat (MyCrawls): improve crawl edit form
    
    * feat (MyCrawls): send edited crawl to backend (not working yet)
    
    * fix (MyCrawls): period is rendering in sec, min, h
    
    * feat (MyCrawls): edit crawl (v1.0 - working)
    
    * style (edit popup): add transition effect
    
    * feat (MyCrawls): add delete crawl popup (not action yet)
    
    * style (crawl icons): add round icon style
    
    * fix(crawling): fix timeout when adding crawl
    
    * feat(crawl CRUD): add deleting crawls
    
    * fix(crawl CRUD): fix response model in delete_crawl
    
    * feat(crawl CRUD): check owner before deleting crawl
    
    * feat (MyCrawls): deleting crawl
    
    Co-authored-by: Adrian Płatos <adiplatos@gmail.com>
    Co-authored-by: Bartlomiej Swoboda <bartlomiejswoboda@gmail.com>
    Co-authored-by: Mjasianek <jasianek.magdalena@gmail.com>

--- frontend/src/components/ContentPages/MyCrawlsPage/MyCrawlsContent.js
+++ frontend/src/components/ContentPages/MyCrawlsPage/MyCrawlsContent.js
@@ -1,11 +1,68 @@
-import React from "react"
+import React, {Component} from "react"
+import Crawl from "./Crawl"
 
-function MyCrawlsContent() {
-    return (
-        <div className="PageContent">
-            
-        </div>
-)
+class MyCrawlsContent extends Component {
+    constructor() {
+        super()
+        this.state = {
+            loading: false,
+            crawlList: []
+        }
+    }
+
+
+async componentDidMount() {
+    console.log("rozpoczeto")
+
+    this.setState({loading: true})
+
+    const response = await fetch("./api/v1/crawling-data/me")
+    console.log(response)
+    const json = await response.json()
+    console.log(json)
+
+    const response2 = await fetch("./api/v1/user/me")
+    console.log(response2)
+    const json2 = await response2.json()
+    console.log(json2)
+
+    this.setState({
+        loading: false,
+        crawlList: json
+    })
+
+    /*
+    this.setState({
+        loading: false,
+        crawlList: [{"name": "elo", "link": "http://wp.pl", "crawl_id": 1},
+        {"name": "mordo", "link": "http://onet.pl", "crawl_id": 2}
+        ]
+    })
+    */   
+         
+}
+    
+    render() {
+        const crawls =  this.state.crawlList;
+        const crawlsList = crawls.map(crawl => <Crawl 
+                                                key={crawl.crawl_id} 
+                                                id={crawl.crawl_id} 
+                                                name={crawl.name} 
+                                                period={crawl.period} 
+                                                email={crawl.email}
+                                                link={crawl.url} 
+                                                />)
+
+        //const loader = this.state.loading ? "loading..." : this.state.crawlList.link
+        return (
+            <div className="PageContent">
+                <h1>My Crawls</h1>
+                <div className="MyCrawls">
+                    {crawlsList}
+                </div>
+            </div>
+        )
+    }
 }
 
 export default MyCrawlsContent
