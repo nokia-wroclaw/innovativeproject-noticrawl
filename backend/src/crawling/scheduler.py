2e5a8f021db8a47429bb6d71866d1d78b2d3295e
Adrian Płatos <57074117+AndriPL@users.noreply.github.com>
2020-06-16 15:44:12 +0200

    Fix "Crawls scheduler" (#64)
    * feat(user): add (fake) login()
    
    * build: add passlib dependency
    
    * feature(auth): add real login
    
    * feature(auth): add cookies for login route
    
    * feat: add email notifications
    
    * refactor: delete scrapy project files
    
    * docker: add auth secrets
    
    * feat(auth): add auth scheme
    
    * feat(auth): add auth
    
    * feat: add save_to_txt helper function
    
    * feat(auth): add auth to crawling routes
    
    * feat(auth): add auth to statics
    
    * feat: add secrets
    
    * refactor: add email notifications to helpers
    
    * feat: add HTTPException
    
    * refactor: main_notifications cleanup
    
    * feat: add secrets
    
    * fix(auth): throw 401 in bearer_cookie_auth
    
    * refactor(auth): combine verify_token with oauth2_scheme
    
    * refactor: remove scrappy junk
    
    * feat(auth): add logout
    
    * refactor: remove logging
    
    * refactor: add newline in debug.py
    
    * feat: add crawl to database
    
    * feat: get crawls from database
    
    * feat: add read_crawls
    
    * feat: add verify_token dependencies
    
    * feat: add update_crawl
    
    * refactor: cleanup
    
    * refactor: cleanup
    
    * fix(backend): remove excessive auth router in main
    
    * refactor: styling changes in auth_contorller
    
    * fix(crawling): fix adding new crawl
    
    * fix(db): fix relations names to work with pydantic
    
    * fix(crawls): fix getting crawls list
    
    * fix(crawlingCRUD): fix updating a crawl
    
    * refactor(cawrCRUD): remove logger from add_crawl
    
    * refactor(crawling): add api tags to crawling_controller
    
    * fix(crawl CRUD): fix adding crawls
    
    * fix(crawl CRUD): add crawl to user, not email from crawl
    
    * feat: change email style
    
    * fix(crawling): fix timeout when adding crawl
    
    * feat(crawl CRUD): add deleting crawls
    
    * feat(scheduler): add scheduler init
    
    * feat(scheduler): add creating crawls queue
    
    * feat(scheduler): scheduler runs but not in new loop
    
    * fix(scheduler): taking screenshots works and crawls_queue is being updated
    
    * feat(scheduler): update element value in db
    
    * feat(scheduler): send screenshots
    
    * feat: add new email style
    
    * fix(scheduler): fix scheduler sleeping too long and add new email template
    
    * feat(scheduler): remove screenshots after sending email
    
    * fix(scheduler): fix loosing crawls outside the queue
    
    * refactor(scheduler): remove class __Scheduler
    
    * fix(scheduler): delete and update crawls without realoading scheduler
    
    * fix(scheduler): fix adding crawl
    
    Co-authored-by: Bartlomiej Swoboda <bartlomiejswoboda@gmail.com>
    Co-authored-by: Mjasianek <jasianek.magdalena@gmail.com>

--- backend/src/crawling/scheduler.py
+++ backend/src/crawling/scheduler.py
@@ -18,117 +18,125 @@ logger = logging.getLogger("Noticrawl")
 MAX_RUNNING_CRAWLS = settings.as_int("MAX_RUNNING_CRAWLS")
 MAX_WAITING_CRAWLS_QUEUE_SIZE = settings.as_int("MAX_WAITING_CRAWLS_QUEUE_SIZE")
 
-class Scheduler:
-    __scheduler = None
 
-    def __init__(self):
-        if not self.__scheduler:
-            self.__scheduler = self.__Scheduler()
+class Scheduler:
+    __waiting_crawls: asyncio.PriorityQueue
+    __crawls_not_in_queue_num: int
+    __running_crawls_futures = []
+    __crawls_to_update_or_delete = {}
+
+    async def __create(self):
+        self.__waiting_crawls = asyncio.PriorityQueue(
+            maxsize=MAX_WAITING_CRAWLS_QUEUE_SIZE
+        )
+        self.__crawls_not_in_queue_num = 0
+        await self.reload_crawls()
 
     async def run_scheduler(self):
-        await self.__scheduler.create()
-        await self.__scheduler.run()
-
-    async def add_crawl(self, crawl: CrawlData):
-        await self.__scheduler.add_crawl(crawl)
+        await self.__create()
+        await self.__run()
 
     async def reload_crawls(self):
-        await self.__scheduler.reload_crawls()
+        self.__waiting_crawls = asyncio.PriorityQueue(
+            maxsize=MAX_WAITING_CRAWLS_QUEUE_SIZE
+        )
+        while self.__crawls_not_in_queue_num > 0:
+            await self.__waiting_crawls.get()
 
-    class __Scheduler:
-        __waiting_crawls: asyncio.PriorityQueue
-        __running_crawls_num: int
-        __running_crawls_futures = []
-
-        async def create(self):
-            self.__waiting_crawls = asyncio.PriorityQueue(
-                maxsize=MAX_WAITING_CRAWLS_QUEUE_SIZE
-            )
-            self.__running_crawls_num = 0
-            await self.reload_crawls()
-
-
-        async def reload_crawls(self):
-            links = get_all_links()
-            self.__waiting_crawls = asyncio.PriorityQueue(
-                maxsize=MAX_WAITING_CRAWLS_QUEUE_SIZE
-            )
-            for link in links:
-                for script in link.scripts:
-                    await self.add_crawl(
-                        CrawlData(
-                            name=script.script_name,
-                            url=link.url,
-                            period=script.period,
-                            email=script.notifications[0].address,
-                            crawl_id=link.link_id,
-                            xpath=script.instructions,
-                            element_value=script.element_value,
-                        )
+        links = get_all_links()
+        for link in links:
+            for script in link.scripts:
+                await self.add_crawl(
+                    CrawlData(
+                        name=script.script_name,
+                        url=link.url,
+                        period=script.period,
+                        email=script.notifications[0].address,
+                        crawl_id=link.link_id,
+                        xpath=script.instructions,
+                        element_value=script.element_value,
                     )
-
-        async def add_crawl(self, crawl: CrawlData):
-            await self.__waiting_crawls.put((time.time() + crawl.period, crawl))
-
-
-        async def run(self):
-            loop = asyncio.get_event_loop()
-            asyncio.create_task(self.__remove_done_futures())
-            while True:
-                # logger.log(level=logging.DEBUG, msg=f"Number of waiting crawls = {self.__waiting_crawls.qsize()}.")        
-
-                moment_of_exec, crawl = await self.__waiting_crawls.get()
-                time_to_wait = moment_of_exec - time.time()
-                if time_to_wait > 0:
-                    await self.__waiting_crawls.put((moment_of_exec, crawl))
-                    await asyncio.sleep(1)
-                else:
-                    logger.log(level=logging.DEBUG, msg=f"Running check for change for crawl {crawl.name}. Number of running crawls = {len(self.__running_crawls_futures)}.")
-                    await self.__run_check_for_change(crawl, loop)
-
-
-        async def __run_check_for_change(self, crawl, loop):
-            while  len(self.__running_crawls_futures) >= MAX_RUNNING_CRAWLS:
-                await asyncio.sleep(1)
-            self.__running_crawls_futures.append(asyncio.run_coroutine_threadsafe(self.__check_for_change(crawl), loop))
-
-
-        async def __check_for_change(self, crawl):
-            logger.log(level=logging.DEBUG, msg=f"Checking crawl {crawl.name}")
-            current_value = await data_selector(url=crawl.url, xpath=crawl.xpath)
-            if current_value != crawl.element_value:
-                msg = (
-                    "   URL: "
-                    + crawl.url
-                    + "\n"
-                    + "   Old value: "
-                    + crawl.element_value
-                    + "\n"
-                    + "   New value: "
-                    + str(current_value)
                 )
-                logger.log(level=logging.DEBUG, msg="Value changed! \n" + msg)
-                
-                update_element_value_in_db(crawl.crawl_id, current_value)
-                crawl.element_value = current_value
 
-                screenshot_name = crawl.name.replace(" ", "_")
-                screenshot_path = await take_screenshot(crawl.url, filename=screenshot_name)
-                send_email(
-                    crawl.email,
-                    screenshot_path,
-                    f"Your crawl \"{crawl.name}\" has new value!"
-                )
-                os.remove(screenshot_path)
+    async def add_crawl(self, crawl: CrawlData):
+        await self.__waiting_crawls.put((time.time() + crawl.period, crawl))
 
-            await self.__waiting_crawls.put((time.time() + crawl.period, crawl))
+    def update_crawl(self, crawl: CrawlData):
+        self.__crawls_to_update_or_delete.update([(crawl.crawl_id, crawl)])
 
-        async def __remove_done_futures(self):
-            while True:
-                for i, future in enumerate(self.__running_crawls_futures):
-                    if future.done():
-                        self.__running_crawls_futures.pop(i)
+    def delete_crawl(self, crawl_id):
+        self.__crawls_to_update_or_delete.update([(crawl_id, None)])
+
+
+    async def __run(self):
+        loop = asyncio.get_event_loop()
+        asyncio.create_task(self.__remove_done_futures())
+        while True:
+            logger.log(level=logging.DEBUG, msg=f"Number of waiting crawls = {self.__waiting_crawls.qsize()}.")
+
+            moment_of_exec, crawl = await self.__waiting_crawls.get()
+            self.__crawls_not_in_queue_num += 1
+            time_to_wait = moment_of_exec - time.time()
+
+            if crawl.crawl_id in self.__crawls_to_update_or_delete.keys():
+                crawl_name = crawl.name
+                crawl = self.__crawls_to_update_or_delete.pop(crawl.crawl_id)
+                if crawl is None:
+                    logger.log(level=logging.DEBUG, msg=f"Crawl {crawl_name} is deleted.")
+                    continue
+
+            if time_to_wait > 0:
+                await self.__waiting_crawls.put((moment_of_exec, crawl))
+                self.__crawls_not_in_queue_num -=1
                 await asyncio.sleep(1)
+            else:
+                logger.log(level=logging.DEBUG, msg=f"Running check for change for crawl {crawl.name}. Number of running crawls = {len(self.__running_crawls_futures)}.")
+                await self.__run_check_for_change(crawl, loop)
+
+
+    async def __run_check_for_change(self, crawl, loop):
+        while  len(self.__running_crawls_futures) >= MAX_RUNNING_CRAWLS:
+            await asyncio.sleep(1)
+        self.__running_crawls_futures.append(asyncio.run_coroutine_threadsafe(self.__check_for_change(crawl), loop))
+
+
+    async def __check_for_change(self, crawl):
+        logger.log(level=logging.DEBUG, msg=f"Checking crawl {crawl.name}")
+        current_value = await data_selector(url=crawl.url, xpath=crawl.xpath)
+        if current_value != crawl.element_value:
+            msg = (
+                "   Name: "
+                + crawl.name
+                + "\n"
+                + "   Old value: "
+                + crawl.element_value
+                + "\n"
+                + "   New value: "
+                + str(current_value)
+            )
+            logger.log(level=logging.DEBUG, msg="Value changed! \n" + msg)
+
+            update_element_value_in_db(crawl.crawl_id, current_value)
+            crawl.element_value = current_value
+
+            screenshot_name = crawl.name.replace(" ", "_")
+            screenshot_path = await take_screenshot(crawl.url, filename=screenshot_name)
+            send_email(
+                crawl.email,
+                screenshot_path,
+                f"Your crawl \"{crawl.name}\" has new value!"
+            )
+            os.remove(screenshot_path)
+
+        await self.__waiting_crawls.put((time.time() + crawl.period, crawl))
+        self.__crawls_not_in_queue_num -=1
+
+    async def __remove_done_futures(self):
+        while True:
+            for i, future in enumerate(self.__running_crawls_futures):
+                if future.done():
+                    self.__running_crawls_futures.pop(i)
+            await asyncio.sleep(1)
 
 
 def update_element_value_in_db(crawl_id, element_value):
@@ -145,5 +153,6 @@ def update_element_value_in_db(crawl_id, element_value):
 def get_all_links():
     db = next(get_db())
     return db.query(Links).all()
+
 
 scheduler = Scheduler()
